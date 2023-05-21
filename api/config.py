d4651c60fb71dfc7b057e5f96f813dc8e62b20b0
gyuseob908 <justku0908@gmail.com>
2023-05-21 19:17:02 +0900

    remove cached

--- api/config.py
+++
@@ -1,9 +0,0 @@
-USER = 'axolotl'
-PASS = 'password'
-HOST = 'localhost'
-PORT = 3306
-DB = 'axolotl_DB'
-
-DB_URL='mysql+pymysql://{user}:{pw}@{host}:{port}/{db}?charset=utf8mb4'.format(user=USER, pw=PASS, host=HOST, port=PORT, db=DB)
-
-#test
