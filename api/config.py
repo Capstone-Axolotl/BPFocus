210d58a319a669e49719d802597b3e341402879a
gyuseob908 <86643164+gyuseob908@users.noreply.github.com>
2023-05-21 14:12:49 +0900

    Delete config.py

--- api/config.py
+++
@@ -1,9 +0,0 @@
-db = {
-    'user':'axolotl',
-    'password':'P@ssw0rd',
-    'host': 'localhost',
-    'port': 3306,
-    'database': 'axolotl_DB'
-}
-DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8mb4"
-
